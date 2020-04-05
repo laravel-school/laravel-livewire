5810f1c3d861c4467d1cefc8e1c1d0df15793dd4
tisuchi <tisuchi@gmail.com>
2020-04-05 20:20:49 +0600

    Add pagination.

--- app/Http/Livewire/PostIndex.php
+++ app/Http/Livewire/PostIndex.php
@@ -4,10 +4,12 @@ namespace App\Http\Livewire;
 
 use App\Post;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class PostIndex extends Component
 {
-    public $posts;
+    use WithPagination;
+    // public $posts;
 
     protected $listeners = [
         'postAdded',
@@ -15,16 +17,19 @@ class PostIndex extends Component
 
     public function postAdded($postId)
     {
-        $this->posts->prepend(Post::find($postId));
+        // $this->posts->prepend(Post::find($postId));
     }
 
     public function mount()
     {
-        $this->posts = Post::latest()->get();
+        // $this->posts = Post::latest()->paginate(3);
     }
 
     public function render()
     {
-        return view('livewire.post-index');
+        return view('livewire.post-index')
+            ->with([
+                'posts' => Post::latest()->paginate(3)
+            ]);
     }
 }
--- resources/views/livewire/post-index.blade.php
+++ resources/views/livewire/post-index.blade.php
@@ -4,4 +4,6 @@
     @foreach($posts as $post)
         <livewire:post-single :post="$post" :key="$post->id">
     @endforeach
+
+    {{ $posts->links() }}
 </div>
